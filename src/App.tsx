bf44476911a9fd897ffe9c8b70c1ffc43c0ac184
Cathy <cathy.healey@gmail.com>
2022-06-01 09:56:00 -0400

    google maps commute

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useJsApiLoader } from "@react-google-maps/api";
+import Map from "./components/Map";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.scss";
+
+const App = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY || "",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+};
 
 export default App;
---
+++ src/components/Map.tsx
@@ -0,0 +1,152 @@
+import { useState, useMemo, useCallback, useRef } from "react";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
+
+import Places from "./Places";
+
+import Distance from "./Distance";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type DirectionsResult = google.maps.DirectionsResult;
+type MapOptions = google.maps.MapOptions;
+
+const Map = () => {
+  const [office, setOffice] = useState<LatLngLiteral>();
+  const [directions, setDirections] = useState<DirectionsResult>();
+  const mapRef = useRef<GoogleMap>();
+  const center = useMemo<LatLngLiteral>(
+    () => (office ? office : { lat: 43.45, lng: -80.49 }),
+    [office]
+  );
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: "b181cac70f27f5e6",
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onLoad = useCallback((map: any) => (mapRef.current = map), []);
+  const houses = useMemo(() => generateHouses(center), [center]);
+  const findDirections = (house: LatLngLiteral) => {
+    if (!houses) return;
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: house,
+        destination: office as LatLngLiteral,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+        }
+      }
+    );
+  };
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Commute</h1>
+        <Places
+          setOffice={(position) => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {!office && <p>Enter the address of your office.</p>}
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={office ? office : center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+          {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#1976D2",
+                  strokeWeight: 5,
+                },
+              }}
+            />
+          )}
+          {office && (
+            <>
+              <Marker position={office} />
+              <MarkerClusterer>
+                {(clusterer) =>
+                  houses.map((house) => (
+                    <Marker
+                      key={house.lat}
+                      position={house}
+                      clusterer={clusterer}
+                      onClick={() => findDirections(house)}
+                    />
+                  )) as any
+                }
+              </MarkerClusterer>
+
+              <Circle center={office} radius={15000} options={closeOptions} />
+              <Circle center={office} radius={30000} options={middleOptions} />
+              <Circle center={office} radius={45000} options={farOptions} />
+            </>
+          )}
+        </GoogleMap>
+      </div>
+    </div>
+  );
+};
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+const closeOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A",
+};
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D",
+};
+const farOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252",
+};
+
+const generateHouses = (position: LatLngLiteral) => {
+  const _houses: Array<LatLngLiteral> = [];
+  for (let i = 0; i < 100; i++) {
+    const direction = Math.random() < 0.5 ? -2 : 2;
+    _houses.push({
+      lat: position.lat + Math.random() / direction,
+      lng: position.lng + Math.random() / direction,
+    });
+  }
+  return _houses;
+};
+export default Map;
